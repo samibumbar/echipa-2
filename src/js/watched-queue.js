99dd5d20dd14c07d0df622b5c768229755c26b2f
Reut Mihai <reutmihai@icloud.com>
2024-09-14 11:54:29 +0300

    update load and link to page2

--- src/js/watched-queue.js
+++ src/js/watched-queue.js
@@ -1,5 +1,6 @@
 const watchedBtn = document.querySelector('.watched');
 const queueBtn = document.querySelector('.queue');
+const STORAGE_KEY = 'movies';
 
 watchedBtn.addEventListener('click', salut);
 
