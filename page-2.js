0300a7e2086cfe2858483f55d3ca08dcad0fe113
samibumbar <134549221+samibumbar@users.noreply.github.com>
2024-09-15 06:46:43 +0000

    Deploying to gh-pages from @ samibumbar/echipa-2@e187c332a1a87cb0e95fe60098e11e41e5a8393c 🚀

--- page-2.js
+++ page-2.js
@@ -1,2 +1,2 @@
-import"./assets/main-C7lW2rMS.js";/* empty css                      */const e=document.querySelector(".watched");document.querySelector(".queue");e.addEventListener("click",t);function t(){console.log("Salut!")}
+import"./assets/main-C5zE2kCR.js";/* empty css                      */const e=document.querySelector(".watched");document.querySelector(".queue");e.addEventListener("click",t);function t(){console.log("Salut!")}
 //# sourceMappingURL=page-2.js.map
