a618ad2504a3c6043d6ae74efcb76a0c80042c25
samibumbar <134549221+samibumbar@users.noreply.github.com>
2024-09-19 22:01:48 +0000

    Deploying to gh-pages from @ samibumbar/echipa-2@d3e3d14ecbd2a55309f9884a72b930f5af05f5e2 🚀

--- assets/main-BsIMWlOe.js
+++
@@ -1,2 +0,0 @@
-document.addEventListener("DOMContentLoaded",()=>{const d=document.getElementById("default-display_gallery"),n=document.getElementById("close-modal-btn");document.querySelector(".modal-content");const e=document.getElementById("movieModal");d.addEventListener("click",()=>{e.classList.remove("is-hidden")}),n.addEventListener("click",t=>{t.stopPropagation,e.style.visibility="hidden",console.log("Modal should be hidden now")}),e.addEventListener("click",t=>{t.target===e&&e.classList.add("is-hidden")})});
-//# sourceMappingURL=main-BsIMWlOe.js.map
