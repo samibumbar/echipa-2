b7aae84c0779de93364e2588c1ff29b9e5b8d79f
Samuel Bumbar <134549221+samibumbar@users.noreply.github.com>
2024-09-22 21:44:29 +0200

    login log out

---
+++ src/js/loin.js
@@ -0,0 +1,87 @@
+import { initializeApp } from 'firebase/app';
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+} from 'firebase/auth';
+
+// Configurația Firebase
+const firebaseConfig = {
+  apiKey: 'AIzaSyCw7VkecxwaMQ6UI5ymBFqf54FHRi8_Fkc',
+  authDomain: 'filmoteka-ef771.firebaseapp.com',
+  projectId: 'filmoteka-ef771',
+  storageBucket: 'filmoteka-ef771.appspot.com',
+  messagingSenderId: '731162565659',
+  appId: '1:731162565659:web:da75ae92afc254876bacbe',
+  measurementId: 'G-13YE8DDH34',
+};
+
+// Inițializează aplicația Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+// Comută între Login și Register
+document.getElementById('show-register').addEventListener('click', event => {
+  event.preventDefault();
+  document.getElementById('login-form').classList.add('form-hidden');
+  document.getElementById('login-form').classList.remove('form-visible');
+  document.getElementById('register-form').classList.remove('form-hidden');
+  document.getElementById('register-form').classList.add('form-visible');
+});
+
+document.getElementById('show-login').addEventListener('click', event => {
+  event.preventDefault();
+  document.getElementById('register-form').classList.add('form-hidden');
+  document.getElementById('register-form').classList.remove('form-visible');
+  document.getElementById('login-form').classList.remove('form-hidden');
+  document.getElementById('login-form').classList.add('form-visible');
+});
+
+// Autentificare utilizator
+document.getElementById('login-btn').addEventListener('click', event => {
+  event.preventDefault();
+  const email = document.getElementById('email-input').value;
+  const password = document.getElementById('password-input').value;
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then(userCredential => {
+      Swal.fire('Logare reușită!', 'Veți fi redirecționat.', 'success');
+      setTimeout(() => {
+        window.location.href = '../index.html';
+      }, 2000);
+    })
+    .catch(error => {
+      Swal.fire('Eroare la logare', error.message, 'error');
+    });
+});
+
+// Înregistrare utilizator
+document.getElementById('register-btn').addEventListener('click', event => {
+  event.preventDefault();
+  const email = document.getElementById('register-email').value;
+  const password = document.getElementById('register-password').value;
+
+  createUserWithEmailAndPassword(auth, email, password)
+    .then(userCredential => {
+      Swal.fire('Înregistrare reușită!', 'Acum vă puteți loga.', 'success');
+      setTimeout(() => {
+        document.getElementById('register-form').classList.add('form-hidden');
+        document
+          .getElementById('register-form')
+          .classList.remove('form-visible');
+        document.getElementById('login-form').classList.remove('form-hidden');
+        document.getElementById('login-form').classList.add('form-visible');
+      }, 2000);
+    })
+    .catch(error => {
+      Swal.fire('Eroare la înregistrare', error.message, 'error');
+    });
+});
+
+// Verificare a autentificării
+onAuthStateChanged(auth, user => {
+  if (user) {
+    window.location.href = '../index.html';
+  }
+});
