61fc02c6b630cbb3165de04cf5b6d88b01373959
Samuel Bumbar <134549221+samibumbar@users.noreply.github.com>
2024-09-24 10:41:30 +0200

    modal responsive

--- src/js/header.js
+++ src/js/header.js
@@ -1,7 +1,62 @@
 // 'use strict';
+const backgroundImages = {
+  desktop: [
+    '/images/desktop/background-red-1x.jpg',
+    '/images/desktop/background-orange-1x.jpg',
+    '/images/desktop/background-yellow-1x.jpg',
+  ],
+  tablet: [
+    '/images/tablet/background-red-1x.jpg',
+    '/images/tablet/background-orange-1x.jpg',
+    '/images/tablet/background-yellow-1x.jpg',
+  ],
+  mobile: [
+    '/images/mobile/bgd-mobil-red-1x.jpg',
+    '/images/mobile/bgd-mobil-orange-1x.jpg',
+    '/images/mobile/bgd-mobil-yellow-1x.jpg',
+  ],
+};
+let currentBackgroundIndex = 0;
+let currentDevice = 'desktop'; // Setează implicit dispozitivul la desktop
+
+function rotateBackground() {
+  const headerContainer = document.querySelector('.header-container');
+
+  // Detectarea dispozitivului pe baza dimensiunii ferestrei
+  if (window.innerWidth >= 1200) {
+    currentDevice = 'desktop';
+  } else if (window.innerWidth >= 768 && window.innerWidth < 1200) {
+    currentDevice = 'tablet';
+  } else {
+    currentDevice = 'mobile';
+  }
+
+  // index aleatoriu pentru imaginea curentă
+  let randomIndex = Math.floor(
+    Math.random() * backgroundImages[currentDevice].length
+  );
+
+  // Evită repetarea aceleași imagini consecutiv
+  while (randomIndex === currentBackgroundIndex) {
+    randomIndex = Math.floor(
+      Math.random() * backgroundImages[currentDevice].length
+    );
+  }
+
+  // Actualizează indexul curent și imaginea de fundal
+  currentBackgroundIndex = randomIndex;
+  headerContainer.style.backgroundImage = `url('${backgroundImages[currentDevice][randomIndex]}')`;
+}
+
+// Apel inițial pentru a seta imaginea de fundal
+rotateBackground();
+
+// Interval pentru a schimba imaginea în mod regulat
+setInterval(rotateBackground, 3000); // Schimbă imaginea la fiecare 3 secunde
+// Schimbă imaginea la fiecare 3 secunde
+
 import { initializeApp } from 'firebase/app';
 import { getAuth, signOut, onAuthStateChanged } from 'firebase/auth';
-
 
 // Configurația Firebase
 const firebaseConfig = {
@@ -34,8 +89,6 @@ document.getElementById('logout-btn').addEventListener('click', () => {
       console.error('Eroare la deconectare:', error);
     });
 });
-
-// const backgroundImages = {
 document.addEventListener('click', function (e) {
   if (e.target && e.target.id === 'logout-btn') {
     signOut(auth)
@@ -62,7 +115,7 @@ const options = {
   autoMatchOsTheme: true, // default: true
 };
 
-const darkmode = new Darkmode(options);
+const darkmode = new darkmode(options);
 darkmode.showWidget();
 
 function saveDarkModePreference(isDarkMode) {
@@ -72,67 +125,12 @@ function saveDarkModePreference(isDarkMode) {
 document
   .querySelector('.darkmode-toggle')
   .addEventListener('click', function () {
-    const isDarkMode = darkmode.isActivated();
+    const isDarkMode = Darkmode.isActivated();
     saveDarkModePreference(isDarkMode);
   });
 
 // Aplică tema pe baza valorii din localStorage la fiecare încărcare de pagină
 if (localStorage.getItem('darkmode') === 'enabled') {
   const darkmode = new Darkmode();
-  darkmode.toggle(); 
+  darkmode.toggle();
 }
-const backgroundImages = {
-  desktop: [
-    '/images/desktop/background-red-1x.jpg',
-    '/images/desktop/background-orange-1x.jpg',
-    '/images/desktop/background-yellow-1x.jpg',
-  ],
-  tablet: [
-    '/images/tablet/background-red-1x.jpg',
-    '/images/tablet/background-orange-1x.jpg',
-    '/images/tablet/background-yellow-1x.jpg',
-  ],
-  mobile: [
-    '/images/mobile/bgd-mobil-red-1x.jpg',
-    '/images/mobile/bgd-mobil-orange-1x.jpg',
-    '/images/mobile/bgd-mobil-yellow-1x.jpg',
-  ],
-};
-
-let currentBackgroundIndex = 0;
-let currentDevice = 'desktop'; // Setează implicit dispozitivul la desktop
-
-function rotateBackground() {
-  const headerContainer = document.querySelector('.header-container');
-
-  // Detectarea dispozitivului (poate fi optimizată)
-  if (window.innerWidth >= 768) {
-    currentDevice = 'desktop';
-  } else if (window.innerWidth >= 480) {
-    currentDevice = 'tablet';
-  } else {
-    currentDevice = 'mobile';
-  }
-
-  // index aleatoriu pentru imaginea curentă
-  let randomIndex = Math.floor(
-    Math.random() * backgroundImages[currentDevice].length
-  );
-
-  // imaginea nu se repetă imediat
-  while (randomIndex === currentBackgroundIndex) {
-    randomIndex = Math.floor(
-      Math.random() * backgroundImages[currentDevice].length
-    );
-  }
-
-  // Actualizează indexul curent și selectează noua imagine
-  currentBackgroundIndex = randomIndex;
-  headerContainer.style.backgroundImage = `url('${backgroundImages[currentDevice][randomIndex]}')`;
-}
-
-// Apel inițial pentru a seta imaginea de fundal
-rotateBackground();
-
-// Interval pentru a schimba imaginea în mod regulat
-setInterval(rotateBackground, 3000); // Schimbă imaginea la fiecare 3 secunde
